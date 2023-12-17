46043e925029c754392901e98c373d570a895d42
mazdul1000 <mazdul1000@gmail.com>
2023-12-17 15:24:24 +0600

    [DONE]MH-24:Setup cors and parser

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,16 @@
-import express from 'express'
+import express, { Application, Request, Response } from 'express'
+import cors from 'cors';
 
-const app = express()
+const app:Application = express()
+app.use(cors());
+
+// parser
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+app.get('/', (req:Request, res:Response) => {
+  res.send('Content Management Application is running successfully');
 })
 
 export default app
